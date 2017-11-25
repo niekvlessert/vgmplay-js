87f37f70bc02efe17c695055150ade519a70d808
Niek Vlessert <niekvlessert@gmail.com>
2017-11-25 11:37:25 +0100

    Fixes
    - Firefox should work now
    - Set correct samplerate for playing device
    - CMakelists.txt cleanups
    - Index.html has some buttons now

--- src/main.c
+++ src/main.c
@@ -4,6 +4,8 @@
 #include "../modules/vgmplay/VGMPlay/VGMPlay.h"
 #include "../modules/vgmplay/VGMPlay/VGMPlay_Intf.h"
 
+extern UINT32 SampleRate;
+
 int main(int argc, char *argv[]) {return 0;}
 
 int FillBuffer2(INT16* Left, INT16* Right){
@@ -14,4 +16,9 @@ int FillBuffer2(INT16* Left, INT16* Right){
                 Right[i]=sample_buffer[i].Right;
         }
         return 1;
+}
+
+int SetSampleRate(UINT32 _SampleRate){
+	SampleRate = _SampleRate;
+	return 1;
 }
